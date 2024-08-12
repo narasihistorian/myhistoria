31b55e05428398a091b6c8952978b1ab3380225c
narasihistorian <narasihistorian@gmail.com>
2024-08-12 22:20:01 +0700

    fix navbar 12 agustus 2024

--- app/lib/routesNavbar.js
+++ app/lib/routesNavbar.js
@@ -1,15 +1,15 @@
 export const routes = [
   {
-    name: 'Home',
-    path: '/',
+    name: "Home",
+    path: "/",
   },
   {
-    name: 'Kategori',
-    path: '/kategori/all',
+    name: "Kategori",
+    path: "/kategori/all",
   },
   {
-    name: 'Video',
-    path: 'https://www.youtube.com/channel/UCNoUf4xYawhvK6dD94oDEDg',
-    target: '_blank',
+    name: "Video",
+    path: "https://www.youtube.com/channel/UCNoUf4xYawhvK6dD94oDEDg",
+    target: "_blank",
   },
 ];
