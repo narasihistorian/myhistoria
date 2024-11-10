88265ffd370a7db26afe4537bbe9499bd6d022c2
Agung Rosyandi <agungrosyandi@gmail.com>
2024-11-10 18:45:31 +0700

    commit git to update local files

--- app/page.js
+++ app/page.js
@@ -2,10 +2,16 @@ import App from "./components/App";
 
 export default function Home() {
   return (
+<<<<<<< HEAD
     <>
       <main className="">
         <App />
       </main>
     </>
+=======
+    <main>
+      <HomeSection />
+    </main>
+>>>>>>> 73519a2 (commit git to update local files)
   );
 }
