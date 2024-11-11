0de1bfb0e8384be76c27f1639b664a958d49d2f6
agung rosyandi <agungrosyandi@gmail.com>
2024-11-12 03:23:22 +0700

    fix update 12 november 2024

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,19 +1,5 @@
-"use client";
-
 import Home from "./Home";
-import { useEffect } from "react";
-import Lenis from "lenis";
 
 export default function App() {
-  const lenis = new Lenis({
-    autoRaf: true,
-  });
-
-  useEffect(() => {
-    lenis.on("scroll", (e) => {
-      console.log(e);
-    });
-  }, []);
-
   return <Home />;
 }
